fe567325d3c4c863dbacabb65345ae50c2b11178
Gabriel <gokba27@gmail.com>
2023-02-14 13:19:30 +0100

    des fonctions

--- premier_programme/Program.cs
+++ premier_programme/Program.cs
@@ -1,29 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Xml.Linq;
 
 namespace premier_programme // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static int demanderAge()
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-            string name = "";
-           while(name.Length < 3)
-            {
-                // Méthode afficahnt un string : 
-                Console.WriteLine("Quel est ton nom ?");
-
-                // Méthode permettant de saisir un string directement dans la console :
-                name = Console.ReadLine();
-                name = name.Trim();
-                if(name.Length < 3)
-                {
-                    Console.WriteLine("Votre nom doit comporter au moins 3 caractères !");
-                }
-            }
-            
             int age_int = 0;
 
             while (age_int <= 0)
@@ -32,17 +17,15 @@ namespace premier_programme // Note: actual namespace depends on the project nam
 
                 string age_str = Console.ReadLine();
 
-                
-
                 try
                 {
                     // int.Parse() permet de convertir un nombre initialement en string en int :
                     age_int = int.Parse(age_str);
-                    if(age_int < 0)
+                    if (age_int < 0)
                     {
                         Console.WriteLine("Votre âge ne doit pas être négatif !");
                     }
-                    if(age_int == 0)
+                    if (age_int == 0)
                     {
                         Console.WriteLine("L'âge ne doit pas être égal à zéro !");
                     }
@@ -51,13 +34,42 @@ namespace premier_programme // Note: actual namespace depends on the project nam
                 {
                     Console.WriteLine("Erreur !! Vous devez rentrer un âge valide !");
                 }
+
             }
+            return age_int;
+        }
+        static string demanderNom()
+        {
+            string name = "";
+            while (name.Length < 3)
+            {
+                // Méthode afficahnt un string : 
+                Console.WriteLine("Quel est ton nom ?");
+
+                // Méthode permettant de saisir un string directement dans la console :
+                name = Console.ReadLine();
+                name = name.Trim();
+                if (name.Length < 3)
+                {
+                    Console.WriteLine("Votre nom doit comporter au moins 3 caractères !");
+                }
+            }
+            int age_int = demanderAge();
             Console.WriteLine("Bonjour et bienvenue " + name + " !" + " Vous avez " + age_int + " ans.");
 
             int age_prochain = age_int + 1;
             Console.WriteLine("Vous aurez bientôt " + age_prochain + " ans.");
+            return name;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string name = demanderNom();
         }
     }
- }
+}
 
     
